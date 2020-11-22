f7ac4ca54ce1ddbab42f7afb04a2b472a72e8154
Hwna <ha07058@kookmin.ac.kr>
2020-11-22 23:18:20 +0900

    Update bbc

--- 21_bbc_20203157/21_bbc_20203157.ino
+++ 21_bbc_20203157/21_bbc_20203157.ino
@@ -24,7 +24,24 @@ void setup() {
   
 // initialize serial port
   Serial.begin(115200);
+}
 
+void loop() {
+  dist_raw = ir_distance();
+  dist_ema = (alpha)*dist_raw + (1-alpha)*(dist_ema);
+  
+  delay(20);
+
+  if (dist_ema >= 185) {
+    myservo.writeMicroseconds(1230);
+  } else {
+    myservo.writeMicroseconds(1630);
+  }
+
+  Serial.print("min:0,max:500,dist:");
+  Serial.print(dist_raw);
+  Serial.print(" ,dist_ema:");
+  Serial.println(dist_ema);
 }
 
 float ir_distance(void){ // return value unit: mm
@@ -32,18 +49,4 @@ float ir_distance(void){ // return value unit: mm
   float volt = float(analogRead(PIN_IR));
   val = ((6762.0/(volt-9.0))-4.0) * 10.0;
   return val;
-}
-
-void loop() {
-  dist_raw = ir_distance();
-  dist_ema = (alpha)*dist_raw + (1-alpha)*(dist_ema);
-  Serial.print("min:0,max:500,dist:");
-  Serial.println(dist_ema);
-  delay(20);
-
-  if (dist_ema >= 185) {
-    myservo.writeMicroseconds(1200);
-  } else {
-    myservo.writeMicroseconds(1600);
-  }
 }
